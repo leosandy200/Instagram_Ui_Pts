1a5c3f928a5a6d57b514364889617a0d73645d11
Leosandy <leosandy595@gmail.com>
2021-03-23 19:25:35 +0700

    update

--- app/src/main/java/com/example/instagram_ui_pts/MainActivity.java
+++ app/src/main/java/com/example/instagram_ui_pts/MainActivity.java
@@ -9,16 +9,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-        EditText email;
-        EditText password;
-        TextView btn;
+    EditText email;
+    EditText password;
+    TextView btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-
 
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
@@ -27,17 +26,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (email.getText().toString().equals("Leosandy") &&
-                password.getText().toString().equals("wahyurismono")) {
+                        password.getText().toString().equals("wahyurismono")) {
                     Toast.makeText(MainActivity.this, "Sukses Login", Toast.LENGTH_LONG).show();
-                }else {
-                    Toast.makeText(MainActivity.this,"Toast",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Toast", Toast.LENGTH_LONG).show();
                 }
             }
         });
 
 
     }
-
 
 
 }
